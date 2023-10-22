77b80cdf6f49373080dcd299f6458b7c51c793d9
Lars Norberg <github@larsnorbergofficial.com>
2023-10-22 14:47:01 +0200

    restructure for menu and embeds

--- Bagnon_ItemInfo/Defaults.lua
+++ Bagnon_ItemInfo/Defaults.lua
@@ -28,6 +28,13 @@ if (Private.Incompatible) then
 	return
 end
 
+-- Addon Localization
+local L = LibStub("AceLocale-3.0"):GetLocale((...))
+
+-- Libraries
+local AceConfigDialog = LibStub("AceConfigDialog-3.0")
+local AceConfigRegistry = LibStub("AceConfigRegistry-3.0")
+
 BagnonItemInfo_DB = {
 	enableItemLevel = true,
 	enableItemBind = true,
@@ -35,6 +42,86 @@ BagnonItemInfo_DB = {
 	enableUncollected = true,
 	enableRarityColoring = true
 }
+
+local setter = function(info,val)
+	BagnonItemInfo_DB[info[#info]] = val
+	if (Private.Forceupdate) then
+		Private.Forceupdate()
+	end
+end
+
+local getter = function(info)
+	return BagnonItemInfo_DB[info[#info]]
+end
+
+local optionDB = {
+	type = "group",
+	args = {
+		header = {
+			order = 1,
+			type = "header",
+			name = ""
+		},
+		enableItemLevel = {
+			order = 10,
+			name = L["Show item levels"],
+			desc = L["Toggle labels showing the item level of the item and the number of slots on containers."],
+			width = "full",
+			type = "toggle",
+			set = setter,
+			get = getter
+		},
+		enableItemBind = {
+			order = 20,
+			name = L["Show unbound items"],
+			desc = L["Toggle labels on items that are not yet bound to you."],
+			width = "full",
+			type = "toggle",
+			set = setter,
+			get = getter
+		},
+		enableRarityColoring = {
+			order = 30,
+			name = L["Colorize item labels by Rarity"],
+			desc = L["Colorize the item level and item bind labels in the item's rarity."],
+			width = "full",
+			type = "toggle",
+			disabled = function(info) return not BagnonItemInfo_DB.enableItemLevel and not BagnonItemInfo_DB.enableItemBind end,
+			set = setter,
+			get = getter
+		},
+		enableGarbage = {
+			order = 40,
+			name = L["Desaturate garbage items"],
+			desc = L["Desaturate and tone down the brightness of garbage items."],
+			width = "full",
+			type = "toggle",
+			set = setter,
+			get = getter
+		},
+		enableUncollected = {
+			order = 50,
+			name = L["Show uncollected appearances"],
+			desc = L["Put a purple eye on uncollected transmog appearances."],
+			width = "full",
+			type = "toggle",
+			disabled = function(info) return not Private.IsRetail end,
+			hidden = function(info) return not Private.IsRetail end,
+			set = setter,
+			get = getter
+		},
+		footer = {
+			order = 10000,
+			type = "header",
+			name = ""
+		}
+	}
+}
+
+local addonName = string.gsub(Addon, "_", " ")
+
+AceConfigRegistry:RegisterOptionsTable(addonName, optionDB)
+AceConfigDialog:SetDefaultSize(addonName, 400, 220)
 
 SLASH_BAGNON_ITEMLEVEL1 = "/bif"
 SlashCmdList["BAGNON_ITEMLEVEL"] = function(msg)
@@ -48,6 +135,13 @@ SlashCmdList["BAGNON_ITEMLEVEL"] = function(msg)
 
 	local action, element = string.split(" ", msg)
 	local db = BagnonItemInfo_DB
+
+	if (not action or action == "") then
+		if (AceConfigRegistry:GetOptionsTable(addonName)) then
+			AceConfigDialog:Open(addonName)
+			return
+		end
+	end
 
 	if (action == "enable") then
 		if (element == "itemlevel" or element == "ilvl") then
