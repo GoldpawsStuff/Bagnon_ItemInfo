6b31219752bfebc6e8259a82ac858c84274eeed0
Lars Norberg <github@larsnorbergofficial.com>
2023-10-25 16:40:12 +0200

    update locale

--- Bagnon_ItemInfo/Locale/enUS.lua
+++ Bagnon_ItemInfo/Locale/enUS.lua
@@ -2,11 +2,17 @@ local L = LibStub("AceLocale-3.0"):NewLocale((...), "enUS", true, true)
 
 L["Colorize item labels by Rarity"]= true
 L["Colorize the item level and item bind labels in the item's rarity."]= true
+L["Darken"] = true
+L["Desaturate"] = true
+L["Desaturate and tone down garbage items"] = true
 L["Desaturate and tone down the brightness of garbage items."]= true
 L["Desaturate garbage items"]= true
+L["Level of darkening"] = true
 L["Put a purple eye on uncollected transmog appearances."]= true
+L["Set the opacity of the darkening layer."] = true
 L["Show item levels"]= true
 L["Show unbound items"]= true
 L["Show uncollected appearances"]= true
 L["Toggle labels on items that are not yet bound to you."]= true
 L["Toggle labels showing the item level of the item and the number of slots on containers."]= true
+L["Tone down the brightness of garbage items."] = true
