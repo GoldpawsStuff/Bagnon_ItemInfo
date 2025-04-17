d89f9d28f82232b0d7d09cfc0792a327a59a37df
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-04-17 11:03:45 +0300

    Update ruRU.lua

--- Bagnon_ItemInfo/Locale/ruRU.lua
+++ Bagnon_ItemInfo/Locale/ruRU.lua
@@ -1,2 +1,22 @@
+---Translator ZamestoTV
 local L = LibStub("AceLocale-3.0"):NewLocale((...), "ruRU")
 if (not L) then return end
+
+L["Colorize item labels by Rarity"] = "Окрашивать названия предметов по редкости"
+L["Colorize the item level and item bind labels in the item's rarity."] = "Окрашивать уровень предмета и метки привязки в цвет, соответствующий редкости предмета."
+L["Darken"] = "Затемнение"
+L["Desaturate"] = "Обесцветить"
+L["Desaturate and tone down garbage items"] = "Обесцветить и уменьшить яркость мусорных предметов"
+L["Desaturate and tone down the brightness of garbage items."] = "Обесцветить и уменьшить яркость мусорных предметов."
+L["Desaturate garbage items"] = "Обесцветить мусорные предметы"
+L["Level of darkening"] = "Уровень затемнения"
+L["Minimum item level"] = "Минимальный уровень предмета"
+L["Put a purple eye on uncollected transmog appearances."] = "Добавлять фиолетовый глаз к неполученным трансмогрификациям."
+L["Set the minimum item level to display."] = "Установить минимальный уровень предмета для отображения."
+L["Set the opacity of the darkening layer."] = "Установить прозрачность слоя затемнения."
+L["Show item levels"] = "Показывать уровни предметов"
+L["Show unbound items"] = "Показывать непривязанные предметы"
+L["Show uncollected appearances"] = "Показывать неполученные трансмогрификации"
+L["Toggle labels on items that are not yet bound to you."] = "Включить/отключить метки для предметов, которые ещё не привязаны к вам."
+L["Toggle labels showing the item level of the item and the number of slots on containers."] = "Включить/отключить метки, отображающие уровень предмета и количество ячеек в контейнерах."
+L["Tone down the brightness of garbage items."] = "Уменьшить яркость мусорных предметов."
